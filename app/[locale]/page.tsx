8d32301302f369b84f42002536bee5d641b825b5
Danymuziki90 <danymuziki90@gmail.com>
2026-08-08 17:29:32 +0200

    feat: implement home page layout with localized dynamic hero and section components

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { resolveSiteLocale } from '@/lib/i18n/locale'
 import { getHeroData } from '@/lib/hero/getHeroData'
 
-import HomeHero from '@/components/home/HomeHero'
+import UnifiedHero from '@/components/ui/UnifiedHero'
 import WhoWeAre from '@/components/home/WhoWeAre'
 import ExpertiseServices from '@/components/home/ExpertiseServices'
 import RecentSessions from '@/components/RecentSessions'
@@ -26,7 +26,7 @@ export default async function HomePage({
   return (
     <div className="flex flex-col min-h-screen">
       {/* 1. Hero Section (première impression) */}
-      <HomeHero heroData={heroData} locale={locale} />
+      <UnifiedHero heroData={heroData} locale={locale} />
 
       {/* 2. Présentation rapide ("Who We Are") */}
       <WhoWeAre locale={locale} />
